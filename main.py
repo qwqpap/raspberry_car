a1681a8cb508ccac098603064f64eab448e870bd
qwqpap <798292805@qq.com>
2022-11-26 03:27:00 +0800

    SOME_NEW

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 import pylab
 
-video = cv2.VideoCapture("test_1.mp4")
+video = cv2.VideoCapture("turn.mp4")
 
 
 def calcGrayHist(img):
@@ -57,7 +57,6 @@ def line_finder(img=0):
     img = img[200:900, 100:1820]
 
     ray_img = cv2.cvtColor(img, None, cv2.COLOR_BGR2HSV)
-    # ray_img = cv2.inRange(img, np.array([110, 0, 0]), np.array([140, 255, 255]))
     gray_img = cv2.cvtColor(ray_img, cv2.COLOR_BGR2GRAY)
     gray_img = cv2.GaussianBlur(gray_img, (gs, gs), 1, 2)
     gray_img = cv2.erode(gray_img, (3, 3), iterations=2)
@@ -65,20 +64,24 @@ def line_finder(img=0):
 
     min_dff = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 81, -10)
 
-    djf = min_dff  # + max_djf
-    # djf = ~djf
+    djf = min_dff
 
     qwq, djf = cv2.threshold(djf, 254, 255, cv2.THRESH_BINARY)
-    # djf = cv2.GaussianBlur(djf,(gs, gs), 1, 2)
-    # qwq, djf = cv2.threshold(djf, 254, 255, cv2.THRESH_BINARY)
-    # kernel = np.ones((4, 4), np.uint8)
-    # djf = cv2.dilate(djf, kernel, 1)
-    djf = cv2.GaussianBlur(djf, (gs, gs), 1, 2)
-    djf = cv2.erode(djf, (5, 5), iterations=3)
+    #hsv 筛选
+    color_dist = {'Lower': np.array([0, 11, 178]), 'Upper': np.array([23, 74, 223])}  # 这一坨是hsv 的参数 希望有苦力来调一下
+    gs_frame = cv2.GaussianBlur(img, (5, 5), 0)  # 高斯滤波
+    hsv = cv2.cvtColor(gs_frame, cv2.COLOR_BGR2HSV)  # 颜色空间转换-hsv
+    erode_hsv = cv2.erode(hsv, None, iterations=2)  # 腐蚀
+    hsv_got = cv2.inRange(erode_hsv, color_dist['Lower'], color_dist['Upper'])
 
+    #两个图层之合成
+    hsv_got = ~hsv_got
+    djf = ~djf
+    finally_got = djf + hsv_got
+    finally_got = ~finally_got
     cv2.imshow("origin_pic", img)
-    cv2.imshow("djff", gray_img)
-    cv2.imshow("gs", djf)
+    cv2.imshow("djff", hsv_got)
+    cv2.imshow("gs", finally_got)
     #inRange_hsv = cv2.inRange(erode_hsv, np.array([Hmin, Smin, Vmin]), np.array([Hmax, Smax, Vmax]))
     # end_time = time.time()
 
@@ -86,7 +89,7 @@ def line_finder(img=0):
     # fps = (1 / delta_time)
     # print(fps)
     #下面这个就是画直方图的，
-    calcGrayHist(djf)
+    #calcGrayHist(djf)
 
 
     cv2.waitKey(20) & 0xff
